9d374da140e6b5d09838fe2f3c5f904413025480
Savage-E <vladkotov36@gmail.com>
2023-01-09 23:00:40 +0300

    Style cards

--- Card game/ContentView.swift
+++ Card game/ContentView.swift
@@ -1,14 +1,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var emojis = ["🚗","🚕","🚙","🏎","🚄","🏍","🚒","🚔","🚜","🚍","✈️","🛻","🚛","🚑","🚚","🚲","🚐","🛺","🛵","🚡","🛴","🚌","🚐","🚓"]
+    
+    @State var emojiCount = 6
+    
     var body: some View {
-        HStack(){
-            ForEach(emojis,id:\.self) {emoji in
-                CardView(content: emoji)
+        VStack{
+            ScrollView{
+                LazyVGrid( columns: [GridItem(.adaptive(minimum: 65))]){
+                    ForEach(emojis[0..<emojiCount], id:\.self) { emoji in
+                        CardView(content: emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
             }
 
-
+            Spacer()
+            HStack{
+                remove
+                Spacer()
+                add
+            }
+            .padding(.horizontal)
+            .font(.largeTitle)
+                
+        }.padding(.horizontal)
+    }
+    
+    var remove: some View{
+        Button {
+            if emojiCount > 1 {
+                emojiCount -= 1}}
+         label: {
+            VStack{Image(systemName: "minus.circle")
+            }
+        }
+    }
+    var add: some View{
+        Button {
+            if emojiCount < emojis.count{
+                emojiCount += 1
+            }
+        }   label: {
+            Image(systemName: "plus.circle")
         }
     }
 }
@@ -23,7 +59,7 @@ struct CardView: View{
             let shape = RoundedRectangle(cornerRadius:20)
             if isFaceUp{
                 shape.fill().foregroundColor(.white)
-                shape.stroke(lineWidth: 3)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content).font(.largeTitle)
             } else{
                 shape.fill(.red)
