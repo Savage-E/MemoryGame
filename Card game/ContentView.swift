e5e2c037735786ae702d35dbf92906af1359a385
Savage-E <vladkotov36@gmail.com>
2023-01-10 00:02:20 +0300

    create classes for logic

--- Card game/ContentView.swift
+++ Card game/ContentView.swift
@@ -17,36 +17,12 @@ struct ContentView: View {
                 }
             }
 
-            Spacer()
-            HStack{
-                remove
-                Spacer()
-                add
-            }
-            .padding(.horizontal)
-            .font(.largeTitle)
+ 
                 
         }.padding(.horizontal)
     }
     
-    var remove: some View{
-        Button {
-            if emojiCount > 1 {
-                emojiCount -= 1}}
-         label: {
-            VStack{Image(systemName: "minus.circle")
-            }
-        }
-    }
-    var add: some View{
-        Button {
-            if emojiCount < emojis.count{
-                emojiCount += 1
-            }
-        }   label: {
-            Image(systemName: "plus.circle")
-        }
-    }
+
 }
 
 
---
+++ Card game/EmojiMemoryGame.swift
@@ -0,0 +1,7 @@
+import SwiftUI
+
+class EmojiMemoryGame {
+    private var model: MemoryGame<String> = MemoryGame<String>(numberOfPairsOfCards: 4)
+    
+    
+}
